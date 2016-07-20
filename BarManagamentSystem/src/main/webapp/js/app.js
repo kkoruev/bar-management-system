f115872502a16643db33fa6749b3b71bf2a889c9
Kris <k.koruev@kmail.bg>
2016-07-20 23:51:44 +0300

    Added frontend structure

---
+++ BarManagamentSystem/src/main/webapp/js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var app = angular.module('BarManagementSystem', [ 'ui.bootstrap', 'ngRoute',
+		'ngResource', 'ui.router' ]);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('login-page', {
+        url: '/',
+        templateUrl: 'templates/login-page.html',
+        controller: 'LoginPageController'
+    })
+   .state('order-list', {
+        url: '/order-list',
+        templateUrl: 'templates/order-list.html',
+        controller: 'OrderListController'
+    })
+     
+    .state('order-items', {
+        url: '/order-items',
+        templateUrl: 'templates/order-items.html',
+        controller: 'OrderItemsController'
+    })
+    /*
+    .state('configure-products', {
+        url: '/configure-products',
+        templateUrl: 'templates/configure-products.html',
+        controller: 'ConfigureController',
+        authenticate: false,
+        data: {
+            viewClass: 'body-background-gray' 
+        }
+    })
+    .state('review-part-list', {
+        url: '/part-list',
+        templateUrl: 'templates/part-list.html',
+        controller: 'PartListController',
+        authenticate: false   
+    })
+    .state('forbidden', {
+        url: '/forbidden',
+        templateUrl: 'templates/page-forbidden.html',
+    })*/
+    $urlRouterProvider.otherwise('/');
+}])
