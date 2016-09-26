0a43d80f3e1fd2f7221c8dc4801e317f0b5995a2
Marina <marinaiivanova93@gmail.com>
2016-09-26 08:23:48 +0200

    Added slider to login page

--- BarManagamentSystem/src/main/webapp/app/login/LoginController.js
+++ BarManagamentSystem/src/main/webapp/app/login/LoginController.js
@@ -1,11 +1,50 @@
 'use strict';
 
 app.controller("LoginController", [
-    '$scope', 'LoginService', '$state', 'RouteConstants',
-    function($scope, LoginService, $state, RouteConstants) {
+    '$scope', '$timeout', 'LoginService', '$state', 'RouteConstants',
+    function($scope, $timeout, LoginService, $state, RouteConstants) {
 
-        $scope.userCreadentials;
+        $scope.userCreadentials;        
+        $scope.currentIndex = 0;        
+        $scope.sliderImages = [{
+        	src : "assets/img/login1.jpg" 
+        	}, {
+        	src : "assets/img/login.jpg"
+        }, {
+        	src : "assets/img/login2.jpg"
+        }, {
+        	src : "assets/img/login3.jpg"
+        }
+        ];
+        
+        $scope.next = function() {
+        	$scope.currentIndex < $scope.sliderImages.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+        };
+        
+        $scope.$watch('currentIndex', function() {
+        	$scope.sliderImages.forEach(function(image){
+        		image.visible = false;
+        	});
+        	
+        	$scope.sliderImages[$scope.currentIndex].visible = true;
+        });
+        
+        var timer;
+        var sliderFunc = function() {
+          timer = $timeout(function() {
+            $scope.next();
+            timer = $timeout(sliderFunc, 2000);
+          }, 2000);
+        };
 
+        sliderFunc();
+
+        $scope.$on('$destroy', function() {
+          $timeout.cancel(timer); // when the scope is getting destroyed, cancel the timer
+        });
+        
+        
+        
         $scope.login = function(userCreadentials) {
             LoginService.login(userCreadentials)
             .then((data) => {
